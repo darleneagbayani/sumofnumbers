c9eaa9e2152150880a882f234572e7ad04a71a18
darleneagbayani <31275691+darleneagbayani@users.noreply.github.com>
2017-09-11 15:41:19 -1000

    second complete

--- sumofnumbers.js
+++ sumofnumbers.js
@@ -2,40 +2,40 @@
  * Solving Experience E26.
  * Created by darleneagbayani on 9/11/17
  */
-let listOfNum = [1, 2, 3, 4];
 
-function sumFor(listOfNum) {
+let list = [1, 2, 3, 4];
+
+function sumFor(list) {
   let sum = 0;
-  for (var i = 0; i < listOfNum.length; i++) {
-    sum += listOfNum[i];
+  for(var i = 0; i < list.length; i++) {
+    sum += list[i];
   }
   return sum;
 }
 
-console.log(sumFor(listOfNum));
+console.log(sumFor(list));
 
-function sumWhile(listOfNum) {
+function sumWhile(list) {
   let sum = 0;
-  while(listOfNum.length > 0) {
-    sum += listOfNum.pop();
+  while(list.length > 0) {
+    sum += list.pop();
   }
   return sum;
 }
 
-console.log(sumWhile(listOfNum));
+console.log(sumWhile(list));
 
-function sumRecursion(listOfNum) {
-  let sum = 0;
-  if(listOfNum.length === 0) {
+function sumRecursion(list) {
+  if(list.length === 0) {
     return 0;
   }
-  return listOfNum[0] + sumRecursion(listOfNum.slice(1, listOfNum.length));
+  return list[0] + sumRecursion(list.slice(1, list.length));
 }
 
 console.log(sumRecursion([1, 2, 3, 4]));
 
-function sumTheSimpleWay(listOfNum) {
-  return _.reduce(listOfNum, function(memo, num) {return memo + num; }, 0);
+function sumTheSimpleWay(list) {
+  return _.reduce(list, function(memo, num) {return memo + num; }, 0);
 }
 
 console.log(sumTheSimpleWay([1, 2, 3, 4]));
