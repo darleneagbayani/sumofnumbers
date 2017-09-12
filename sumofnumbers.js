8797bc0efccfbeaa7569c981f18c706b9ac296f2
darleneagbayani <31275691+darleneagbayani@users.noreply.github.com>
2017-09-11 15:30:49 -1000

    complete

--- sumofnumbers.js
+++ sumofnumbers.js
@@ -2,26 +2,40 @@
  * Solving Experience E26.
  * Created by darleneagbayani on 9/11/17
  */
-let sumOfNum = 0;
+let listOfNum = [1, 2, 3, 4];
 
 function sumFor(listOfNum) {
+  let sum = 0;
   for (var i = 0; i < listOfNum.length; i++) {
-    sumOfNum += listOfNum[i];
+    sum += listOfNum[i];
   }
-  return sumOfNum;
+  return sum;
 }
+
 console.log(sumFor(listOfNum));
 
 function sumWhile(listOfNum) {
-  console.log(sumOfNum);
+  let sum = 0;
+  while(listOfNum.length > 0) {
+    sum += listOfNum.pop();
+  }
+  return sum;
 }
+
+console.log(sumWhile(listOfNum));
 
 function sumRecursion(listOfNum) {
-  console.log(sumOfNum);
+  let sum = 0;
+  if(listOfNum.length === 0) {
+    return 0;
+  }
+  return listOfNum[0] + sumRecursion(listOfNum.slice(1, listOfNum.length));
 }
 
-function sumUnderscore(listOfNum) {
-  console.log(sumOfNum);
+console.log(sumRecursion([1, 2, 3, 4]));
+
+function sumTheSimpleWay(listOfNum) {
+  return _.reduce(listOfNum, function(memo, num) {return memo + num; }, 0);
 }
 
-const listOfNum[1, 2, 3, 4];
+console.log(sumTheSimpleWay([1, 2, 3, 4]));
