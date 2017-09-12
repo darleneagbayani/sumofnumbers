a428c09906a5a7224a1821a10497d7ff8504c5c6
darleneagbayani <31275691+darleneagbayani@users.noreply.github.com>
2017-09-11 14:01:47 -1000

    test 1

---
+++ sumofnumbers.js
@@ -0,0 +1,22 @@
+/*
+ * Solving Experience E26.
+ * Created by darleneagbayani on 9/11/17
+ */
+
+function sumFor(listOfNum) {
+  console.log(listOfNum);
+}
+
+function sumWhile(listOfNum) {
+  console.log(listOfNum);
+}
+
+function sumRecursion(listOfNum) {
+  console.log(listOfNum);
+}
+
+function sumUnderscore(listOfNum) {
+  console.log(listOfNum);
+}
+
+const listOfNum({1, 2, 3, 4});
